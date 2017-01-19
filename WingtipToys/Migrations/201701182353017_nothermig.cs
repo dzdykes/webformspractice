dfc46f9399d2aea69e2551ca23eb370cd762b08b
dzdykes <dzdykes88@gmail.com>
2017-01-19 12:53:51 -0800

    first commit

---
+++ WingtipToys/Migrations/201701182353017_nothermig.cs
@@ -0,0 +1,16 @@
+namespace WingtipToys.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class nothermig : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ WingtipToys/Models/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace WingtipToys.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("WingtipToys")
+        {
+        }
+
+        public static ProductContext Create()
+        {
+            return new ProductContext();
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
